b0775e3c49ba936e294f7f1b98b7c4476b70c2f5
bastian <bastian.barrera.r@gmail.com>
2023-06-12 20:14:55 -0400

    added random button func

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -10,12 +10,14 @@ const Main = () => {
   const [cardName, setCardName] = React.useState(null);
   const [cardLvl, setCardLvl] = React.useState(null);
   const [cardState, setCardState] = React.useState(false);
+  const [randomNum, setRandomNum] = React.useState(0)
 
   React.useEffect(() => {
     axios.get("https://digimon-api.vercel.app/api/digimon").then((data) => {
       const digimons = data.data.map((item) => item.name);
       setData(data.data);
       setList(digimons);
+      setRandomNum(Math.floor(Math.random() * 50));
     });
   }, []);
 
@@ -27,11 +29,22 @@ const Main = () => {
     setCardState(true);
   };
 
+  const random = (event) =>{
+    event.preventDefault();
+    setRandomNum(Math.floor(Math.random()*list.length));
+    setCardImg(data[randomNum].img);
+    setCardName(data[randomNum].name);
+    setCardLvl(data[randomNum].level);
+    setCardState(true);
+  }
+
   return (
     <main>
-      {!cardState && <div id="main--logo">
-        <img id="main--logo--img" src={DigiLogo} alt="logo" />
-      </div>}
+      {!cardState && (
+        <div id="main--logo">
+          <img id="main--logo--img" src={DigiLogo} alt="logo" />
+        </div>
+      )}
       {/* dropdown select */}
       <form onSubmit={submit}>
         <select
@@ -48,8 +61,11 @@ const Main = () => {
         </select>
         {/* buttons */}
         <div id="main--buttons">
-          <button type="submit" id="main--buttons-button">
+          <button type="submit" className="main--buttons-button">
             Buscar
+          </button>
+          <button onClick={random} className="main--buttons-button">
+            Random
           </button>
         </div>
       </form>
@@ -57,7 +73,7 @@ const Main = () => {
       {cardState && (
         <div id="card">
           <div id="card--img">
-            <img id="card--img--img" src={cardImg} alt="digimon image" />
+            <img id="card--pic" src={cardImg} alt="digimon image" />
           </div>
           <hr />
           <div id="card--name">{cardName}</div>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,15 +2,16 @@ import React from 'react';
 import Digivice from '../images/digivice.png'
 
 const Navbar = () =>{
-    return(
-        <nav>
-            <img src={Digivice} alt='digilogo' id='nav--img' />
-            <ul>
-                <li>Digimon DB</li>
-                <li>DigiWiki</li>
-            </ul>
-        </nav>
-    )
+    return (
+      <nav>
+        <img src={Digivice} alt="digilogo" id="nav--img" />
+        <ul>
+          <li>
+            <a href="/">Digimon DB</a>
+          </li>
+        </ul>
+      </nav>
+    );
 }
 
 export default Navbar;
